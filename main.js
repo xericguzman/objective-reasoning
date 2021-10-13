a1859f3d35a40e87bcc8080bc87b933a684b93e0
eric guzman <xericguzman@gmail.com>
2021-10-13 10:47:19 -0400

    fin

--- main.js
+++ main.js
@@ -1,6 +1,52 @@
 // ┌─────────────────┐
 // │ Your code here! │	
 // └─────────────────┘
+function isAdmin(object) {
+if (object.userRole ==="ADMIN"){
+    return true;
+} else {
+    return false;
+}
+}
+
+
+function getEmail(user) {
+    firstName = user.firstName.toLowerCase();
+    lastName = lastName = user.lastName.toLowerCase();
+    email = firstName + lastName + '@codeimmersives.com';
+    return email;
+}
+
+function getPlaylistLength(playlist) {
+    return playlist.songs.length
+}
+
+function getHardestHomework(arr) {
+    let lowestScore = 1==
+    let hardestHW = ''
+
+    for (let i = 0; i < arr.length; i++) {
+        const score = arr[i]averageScore;
+
+        if (score < lowestScore) {
+            lowestScore = score
+            hardestHW = arr[i].name 
+        }
+    }
+return hardestHW
+}
+
+function createPhonebook(firstNameArr, numArr, phonebook = {}) {
+    for (let i = 0; i < firstNameArr.length; i++) {
+        let firstName = firstNameArr[i];
+        let ph = numArr[i];
+
+        phonebook[firstName] = ph;
+    }
+    return phonebook;
+}
+
+
 
 
 
